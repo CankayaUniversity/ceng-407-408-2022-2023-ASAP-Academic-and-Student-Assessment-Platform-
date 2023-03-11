7876e9b701de5f1e7723dd05436bc6ed16be8282
Emre Haser <emrehaser@gmail.com>
2023-03-11 19:45:04 +0300

    Drive Upload cs

---
+++ ASAP Project/GoogleDrive.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using static Google.Apis.Drive.v3.DriveService;
+
+namespace ASAP_Project
+{
+    internal class GoogleDrive
+    {
+        public void UploadFile()
+        {
+            string oSelectedFile = "";
+            System.Windows.Forms.OpenFileDialog oDlg = new System.Windows.Forms.OpenFileDialog();
+            if (System.Windows.Forms.DialogResult.OK == oDlg.ShowDialog())
+            {
+                oSelectedFile = oDlg.FileName;
+
+            }
+
+            UserCredential credential;
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    clientSecrets: GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { DriveService.Scope.Drive },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "MyConsoleApp",
+            });
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = "TEST"
+            };
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new FileStream(oSelectedFile, FileMode.Open))
+            {
+                request = service.Files.Create(
+                    fileMetadata, stream, "application/vnd.ms-excel");
+                request.Fields = "id";
+                request.Upload();
+            }
+            var file = request.ResponseBody;
+            Console.WriteLine("File ID: " + file.Id);
+        }
+    }
+}
