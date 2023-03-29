4feae75f3aa183d16d4abbccac299814047804e3
Tan Karabudak <99688778+R3TR05PH3R3@users.noreply.github.com>
2023-03-30 00:59:08 +0300

    delirdim

--- ASAP_Project/MainWindow.xaml.cs
+++ ASAP_Project/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +13,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using Microsoft.Win32;
+using System.IO;
+using ExcelDataReader;
+using System.Data;
 
 namespace ASAP_Project
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
@@ -53,16 +59,16 @@ namespace ASAP_Project
             {
                 grid_generate_excel.Visibility = Visibility.Visible;
             }
-            
+
         }
 
-        
+
         private void textbox_midtermcount_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Label[] midtermlabel = new Label[int.Parse(textbox_midtermcount.Text)];
+            System.Windows.Controls.Label[] midtermlabel = new System.Windows.Controls.Label[int.Parse(textbox_midtermcount.Text)];
             for (int i = 0; i < int.Parse(textbox_midtermcount.Text); i++)
             {
-                midtermlabel[i] = new Label();
+                midtermlabel[i] = new System.Windows.Controls.Label();
                 midtermlabel[i].Name = "qCountmt" + (i + 1);
                 midtermlabel[i].HorizontalAlignment = HorizontalAlignment.Left;
                 midtermlabel[i].VerticalAlignment = VerticalAlignment.Top;
@@ -70,17 +76,17 @@ namespace ASAP_Project
                 midtermlabel[i].Height = 30;
                 midtermlabel[i].Opacity = 0.8;
                 midtermlabel[i].Content = "Question Count Midterm " + (i + 1) + " :";
-                midtermlabel[i].Margin = new Thickness(272, (i*30) + 29, 0, 0);
+                midtermlabel[i].Margin = new Thickness(272, (i * 30) + 29, 0, 0);
                 midtermlabel[i].Foreground = Brushes.White;
                 midtermlabel[i].Visibility = Visibility.Visible;
                 grid_generate_excel.Children.Add(midtermlabel[i]);
             }
 
-            TextBox[] midtermtextbox = new TextBox[int.Parse(textbox_midtermcount.Text)];
+            System.Windows.Controls.TextBox[] midtermtextbox = new System.Windows.Controls.TextBox[int.Parse(textbox_midtermcount.Text)];
             for (int i = 0; i < int.Parse(textbox_midtermcount.Text); i++)
             {
-                midtermtextbox[i] = new TextBox();
-                midtermtextbox[i].Name = "qTextboxmidterm" + (i+1);
+                midtermtextbox[i] = new System.Windows.Controls.TextBox();
+                midtermtextbox[i].Name = "qTextboxmidterm" + (i + 1);
                 midtermtextbox[i].HorizontalAlignment = HorizontalAlignment.Left;
                 midtermtextbox[i].VerticalAlignment = VerticalAlignment.Top;
                 midtermtextbox[i].Width = 70;
@@ -96,10 +102,10 @@ namespace ASAP_Project
             int lastps = int.Parse(textbox_midtermcount.Text);
             int last = lastps * 30;
 
-            Label[] homeworklabel = new Label[int.Parse(textbox_homeworkcount.Text)];
+            System.Windows.Controls.Label[] homeworklabel = new System.Windows.Controls.Label[int.Parse(textbox_homeworkcount.Text)];
             for (int i = 0; i < int.Parse(textbox_homeworkcount.Text); i++)
             {
-                homeworklabel[i] = new Label();
+                homeworklabel[i] = new System.Windows.Controls.Label();
                 homeworklabel[i].Name = "qCountmt" + (i + 1);
                 homeworklabel[i].HorizontalAlignment = HorizontalAlignment.Left;
                 homeworklabel[i].VerticalAlignment = VerticalAlignment.Top;
@@ -113,10 +119,10 @@ namespace ASAP_Project
                 grid_generate_excel.Children.Add(homeworklabel[i]);
             }
 
-            TextBox[] homeworktextbox = new TextBox[int.Parse(textbox_homeworkcount.Text)];
+            System.Windows.Controls.TextBox[] homeworktextbox = new System.Windows.Controls.TextBox[int.Parse(textbox_homeworkcount.Text)];
             for (int i = 0; i < int.Parse(textbox_homeworkcount.Text); i++)
             {
-                homeworktextbox[i] = new TextBox();
+                homeworktextbox[i] = new System.Windows.Controls.TextBox();
                 homeworktextbox[i].Name = "qTextboxmidterm" + (i + 1);
                 homeworktextbox[i].HorizontalAlignment = HorizontalAlignment.Left;
                 homeworktextbox[i].VerticalAlignment = VerticalAlignment.Top;
@@ -144,6 +150,36 @@ namespace ASAP_Project
         {
             GoogleDrive.UploadFile();
         }
-    }
-    
+
+        private void button_ok_Click(object sender, RoutedEventArgs e)
+        {
+
+            var openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filename + ";Extended Properties=\"Excel 12.0;HDR=YES\"";
+                OleDbConnection connection = new OleDbConnection(connectionString);
+                connection.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Course_Grading_Constraints$]", connection);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                adapter.Fill(dt);
+                connection.Close();
+                datagrid_reviewcourse.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        private void button_reviewcourse_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid_reviewcourse.Visibility == Visibility.Visible)
+            {
+                grid_reviewcourse.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                grid_reviewcourse.Visibility = Visibility.Visible;
+            }
+        }
+    } 
 }
