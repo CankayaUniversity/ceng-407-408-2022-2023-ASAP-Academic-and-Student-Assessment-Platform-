5e990fe46b2811df6641526ff832c4bed2f28819
R3TR05PH3R3 <tan.karabudak@gmail.com>
2023-05-29 12:33:25 +0300

    OK NOICE

--- ASAP_Project/LoginWindow.xaml.cs
+++ ASAP_Project/LoginWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ASAP_Project
 {
@@ -29,7 +30,36 @@ namespace ASAP_Project
         {
             
             MemoryStream userdata = GoogleDrive.GetFile("UserInfo");
+            // Converts MemoryStream to byte[]
+            byte[] excelData = userdata.ToArray();
 
+            // Saves byte[] as a temporary file
+            string tempFilePath = System.IO.Path.GetTempFileName();
+            File.WriteAllBytes(tempFilePath, excelData);
+
+            // Opens the temporary file with Excel Interop
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook wb = excelApp.Workbooks.Open(tempFilePath);
+            foreach (Excel.Worksheet worksheet in wb.Sheets)
+            {
+                if (worksheet.Name == "Account")
+                {
+                    for(int i = 2; i < worksheet.Rows.Count; i++)
+                    {
+                        if(worksheet.Cells[i,2] == /*Username*/)
+                        {
+                            if(worksheet.Cells[i,3] == /*password*/)
+                            {
+                                //assigns if the user is a user or an admin
+                                //goes to the main page with username and user type
+                                //in main page we open the specific buttons according to if we have an admin or
+                                //a user on board.
+                                //From Tan to Emre :D
+                            }
+                        }
+                    }
+                }
+            }
 
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
